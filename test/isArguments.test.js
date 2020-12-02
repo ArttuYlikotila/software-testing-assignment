8dc2e04e7807bf64b87f0e62691130418b9ed2df
Nelli Leinonen <leinonen.nelli@gmail.com>
2020-12-02 14:13:53 +0200

    Implement TC-13 and TC-14

---
+++ test/isArguments.test.js
@@ -0,0 +1,15 @@
+import chai from "chai";
+import isArguments from "../src/isArguments.js";
+
+const expect = chai.expect;
+
+// TC-13 and TC-14
+describe("Check if things are arguments objects", () => {
+  it("TC-13: Check if an arguments object returned from a function is an arguments object", () => {
+    expect(isArguments(function () { return arguments }())).to.equal(true)
+  });
+
+  it("TC-14: Check if a string is an arguments object", () => {
+    expect(isArguments('abc')).to.equal(false)
+  });
+})
